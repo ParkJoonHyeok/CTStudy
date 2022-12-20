09b43bac58db35d487774ba8f5c7b5ff6e14e23c
DESKTOP-8N8N4LA\pjhmain <poqwer51@naver.com>
2022-12-21 00:37:37 +0900

    Revert "Revert "백준 단계별 문제 1_2557 풀이""
    This reverts commit a63d93fd66999b3a4d0334141518bc0793a9f88d.

---
+++ BaekJoon_basic/src/step1/No1_2557.java
@@ -0,0 +1,13 @@
+package step1;
+
+public class No1_2557 {
+
+	public static void main(String[] args) {
+		// 문제 : Hello World!를 출력하시오.
+		
+		// 예제 출력 : Hello World!
+		
+		System.out.println("Hello World!");
+	}
+
+}
